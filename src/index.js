74041a55b0c7d288a3ebab30feabf727b229168d
Natito9 <179492180+Natito9@users.noreply.github.com>
2024-11-14 17:27:13 +0100

    updated photos without border

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import './reset.css';
 import './header.css';
 
 
-const menuToggle = document.querySelector('.menu-toggle');
-const menuList = document.querySelector('.menu-list');
+window.addEventListener('scroll', function() {
+    const quoteContainer = document.querySelector('.quote-container');
+    const quote = quoteContainer.querySelector('.quote');
+    const rect = quoteContainer.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
 
-menuToggle.addEventListener('click', () => {
-    menuList.classList.toggle('active'); // Toggles the 'active' class
+    if (rect.top >= 0 && rect.bottom <= windowHeight) {
+        quoteContainer.style.visibility = 'visible'; // Make it visible
+        quote.style.opacity = 1;
+       
+    }
 });
